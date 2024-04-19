6a2ecde3e8ca4f13a80ce8d2c0dcb1d8c99dfd1e
paulgiraldo <150484371+paulgiraldo@users.noreply.github.com>
2024-04-19 14:41:23 -0500

    Actualizacion

---
+++ giraldo-palacios/src/main/java/com/codigo/giraldopalacios/GiraldoPalaciosApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.giraldopalacios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraldoPalaciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraldoPalaciosApplication.class, args);
+	}
+
+}
---
+++ giraldo-palacios/src/test/java/com/codigo/giraldopalacios/GiraldoPalaciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.giraldopalacios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraldoPalaciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
